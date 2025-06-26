62dd1d7da8a6954c6bc9a22b2ae857503d241739
MaxiColman <colmanmaximo2003@gmail.com>
2025-06-25 23:21:47 -0600

    La rutas fueron creadas para enlazarlas con los models para determinar si la información de los personsajes está siendo bien cargada en la base de datos. Al mismo tiempo se realizó las validaciones para la creación del personaje y se agregaron espicificaciones al env.example.

--- src/controllers/character.controllers.js
+++ src/controllers/character.controllers.js
@@ -1,5 +1,48 @@
+import { type } from "os";
 import Character from "../models/character.model.js";
 
 export const createCharacter = async(req, res) => {
     
+    if(req.body){
+        for (let value in req.body){
+            if (typeof req.body[value] === "string"){
+                req.body[value] = req.body[value].thim();
+            }
+        }
+    }
+
+    const {name, ki, race, gender, description} = req.body;
+
+    try {
+        //Validacion para que los datos no se reciban vacios.
+        if(name === undefined) return res.status(400).json({errorMessage: "Debe completar el campo 'name', no puede estar vacio."})
+        if(ki === undefined) return res.status(400).json({errorMessage: "Debe completar el campo'ki ', no puede estar vacio."})
+        if(race === undefined) return res.status(400).json({errorMessage: "Debe completar el campo 'race', no puede estar vacio."})
+        if(gender === undefined) return res.status(400).json({errorMessage: "Debe completar el campo 'gender', no puede estar vacio."})
+
+        //Validación para que los nombres sean únicos y no se repitan para que no haya incongruencias
+        const nameUnique = await Character.findOne({where: {name}});
+
+        if(nameUnique) return res.status(400).json({errorMessage: "'name' el nombre debe ser único por personaje."});
+        
+        //Validación para que el ki sea un número entero y no un número con decimales
+        const kiInt = Math.floor(ki);
+        if(ki !== kiInt) return res.status(400).json({Message: "el ki debe de ser un número entero."});
+
+        //Validación para que el género sea solo Female o Male si o si y que no tenga dos generos.
+        if(!(gender === "Female" || gender === "Male")) return res.status(400).json({errorMessage: "'gender' el genero debe ser 'Femenino' o 'Masculino'."});
+
+        //Si la descripción no viene vacias se valida que sea un string para que se coloque como cadena de texto en la información del personaje
+        if(description !== undefined){
+            if(typeof description !== "string"){
+                return res.status(400).json({errorMessage: "La 'description' debe de ser cadena de texto(caracteres)."});
+            }
+        }
+
+        const character = await Character.create({name, ki, race, gender, description});
+        res.status(200).json({Message: "Personaje creado con éxito: ", character});
+    } catch (error) {
+        console.log("Error en la creación del personaje: ", error)
+        res.status(500).json({Message: error.message});
+    }
 }
